de03caa6aef5999c7cbf2f183b3a60670e3bad88
Neerav <ananthaneerav@gmail.com>
2020-11-07 16:03:04 +0530

    updated bst

--- Datastructures_and_Algorithms_cpp/binary_search_tree.cpp
+++ Datastructures_and_Algorithms_cpp/binary_search_tree.cpp
@@ -22,6 +22,24 @@ BstNode* Insert(BstNode* root,int data)
         root->right = Insert(root->right,data);
     return root;
 }
+int Findmin(BstNode* root)
+{
+    if(root == NULL)
+        throw "Error: bst is Empty";
+    else
+        while(root->left!=NULL)
+            root = root->left;
+    return root->data;
+}
+int Findmax(BstNode* root)
+{
+    if(root==NULL)
+        return false;
+    else
+        while(root->right!=NULL)
+            root=root->right;
+    return root->data;
+}
 bool Search(BstNode* root,int data)
 {
     if(root==NULL)
@@ -38,9 +56,13 @@ int main()
 {
     BstNode* root = NULL;
     root = Insert(root,10);
+    root = Insert(root,15);
+    root = Insert(root,50);
     root = Insert(root,20);
     root = Insert(root,30);
-    root = Insert(root,2);
+    root = Insert(root,40);
     if(Search(root,12)==true)cout<<"Item Found"<<endl;else cout<<"Item not Found"<<endl;
-    if(Search(root,10)==true)cout<<"Item Found";
+    if(Search(root,10)==true)cout<<"Item Found"<<endl;else cout<<"Item not Found"<<endl;
+    cout<<"The minimum element in an BST is: "<<Findmin(root)<<endl;
+    cout<<"The maximum element in an BST is: "<<Findmax(root)<<endl;
 }
